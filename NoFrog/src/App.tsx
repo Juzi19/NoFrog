c5086d8ac54242a6deda047c8dfb05e88579981e
Juzi19 <justus.zimmermann@gmx.de>
2024-08-28 13:17:52 +0200

    Nav Bar first safe
    First draft of the nav bar

--- NoFrog/src/App.tsx
+++ NoFrog/src/App.tsx
@@ -1,23 +1,23 @@
 // src/App.js
-import { Routes, Route, Link } from 'react-router-dom';
-import Home from './components/Home.tsx';
+import { Routes, Route, Link } from 'react-router-dom'
+import Home from './components/Home.tsx'
+import './App.css'
 
 
 function App() {
   return (
     <div>
       <header>
-        <Link to="/"><h1>nofrog</h1></Link>
+        <Link to="/" className="header-link"><h1>nofrog</h1></Link>
         <nav>
-          <Link to="/more">Mehr</Link>
-          <Link to="/aboutus">Über uns</Link>
-          <Link to="/contact">Kontakt</Link>
+          <Link to="/more" className="nav-link ">Mehr</Link>
+          <Link to="/aboutus" className="nav-link">Über uns</Link>
+          <Link to="/contact" className="nav-link">Kontakt</Link>
         </nav>
       </header>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/contact" element={<Contact />} />
+
       </Routes>
     </div>
   );
