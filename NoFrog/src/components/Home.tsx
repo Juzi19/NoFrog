11b96a0e54fced83b30304e66445c104d043c06a
Juzi19 <justus.zimmermann@gmx.de>
2024-08-29 09:43:12 +0200

    first design businesscard and text
    css and html almost done, animations required

--- NoFrog/src/components/Home.tsx
+++ NoFrog/src/components/Home.tsx
@@ -1,7 +1,17 @@
+import './Home.css'
+
 
 function Home() {
   return (
-    <div>Home</div>
+    <>
+    <section id='main'>
+        <div className='h1textcontainer'><h1>Ihre <span>Website</span> ist die <span>digitale Visitenkarte</span>.</h1></div>
+        <div className='h1textcontainer'><h1><span>Perfekt</span> gestaltet, gewinnt sie <span>Kunden</span>.</h1></div>
+        <div className='h1textcontainer'><h1>Alles <span>andere</span> lässt sie <span>gehen</span>.</h1></div>
+      <img src="src/assets/business_card.png" alt="Visitenkarte" className='businesscard'/>  
+      
+    </section>
+    </>
   )
 }
 
